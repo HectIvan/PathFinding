1e47e6337d4f85b1755c79e16e27181de952888f
HectIvan <idv21c.hmunoz@uartesdigitales.edu.mx>
2023-04-28 21:03:40 -0600

    PathFinder
    el algoritmo es capaz de encontrar el camino mas cercano y representarlo visualmente, sin embargo parece que solo toma en cuenta un eje a pesar de introducirse 2, sospecho que será el valor de retorno en la busqueda de la distancia de manhattan

--- Path/Assets/common/scripts/Box.cs
+++ Path/Assets/common/scripts/Box.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class Box : MonoBehaviour
 {
     public GameObject m_prefab;
-    public int m_posX;
-    public int m_posZ;
+    public MeshRenderer m_MR;
+    public float m_posX = 0f;
+    public float m_posZ = 0f;
     public bool m_impassable;
     public bool m_pathMember;
+
+    public Material m_impassableMat;
+    public Material m_neutralMat;
+    public Material m_pathMat;
+
+    public Vector2 m_left       ;//= new Vector2(0,0);
+    public Vector2 m_right      ;//= new Vector2(0,0);
+    public Vector2 m_up         ;//= new Vector2(0,0);
+    public Vector2 m_down       ;//= new Vector2(0,0);
+
+    public Box()
+    {       
+        m_posX = 0f;
+        m_posZ = 0f;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        //this.gameObject.AddComponent<Box>();
+        //m_MR = this.GetComponent<MeshRenderer>();
+        m_left.x = m_posX - 1;
+        m_left.y = m_posZ;
+
+        m_right.x = m_posX + 1;
+        m_right.y = m_posZ;
+
+        m_up.x = m_posX;
+        m_up.y = m_posZ + 1;
+
+        m_down.x = m_posX;
+        m_down.y = m_posZ - 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_pathMember)
+        {
+            //this.GetComponent<MeshRenderer>().material = m_pathMat;
+        }
+        if (m_impassable)
+        {
+            //this.GetComponent<MeshRenderer>().material = m_impassableMat;
+        }
     }
 }
--- Path/Assets/common/scripts/grid.cs
+++ Path/Assets/common/scripts/grid.cs
@@ -5,42 +5,182 @@ using UnityEngine;
 public class grid : MonoBehaviour
 {
     public GameObject m_gridBox;
-    public int m_X;
-    public int m_Y;
+    public int m_X = 0;
+    public int m_Z = 0;
 
-    public int m_objectiveX;
-    public int m_objectiveZ;
+    public int m_objectiveX = 0;
+    public int m_objectiveZ = 0;
 
-    public int m_originX;
-    public int m_originZ;
+    public int m_originX = 0;
+    public int m_originZ = 0;
+    public Vector2 m_originSolid = new Vector2(0, 0);
+    public float m_manhattanDistance = 0f;
+    public Box m_originBox;
+    public Box m_newOriginBox;
 
-    //materials
+    //materialsm_originX
     public Material m_originMat;
     public Material m_objectiveMat;
     public Material m_impassableMat;
     public Material m_routeMat;
     public Material m_neutralMat;
 
+    public int m_f;
+    public int m_g = 1;
+    public int m_h;
+
+
     //list
-    public static List<GameObject> m_gridBoxInstances = new List<GameObject>();
+    public static List<Box> m_gridBoxInstances = new List<Box>();
+    //public static List<Box> m_gridBoxStorage = new List<Box>();
 
     // Start is called before the first frame update
     void Start()
     {
         InstantiateGrid();
-        Search();
+        m_newOriginBox = m_originBox;
+        while (m_originBox.m_posX != m_objectiveX && m_originBox.m_posZ != m_objectiveZ)
+        {
+            FindPath();
+        }
+        // Search();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //FindPath();
+    }
+
+    /*
+      List<Box> boxNeighbours = new List<Box>();
+        Box t_up = gameObject.AddComponent<Box>();
+        boxNeighbours.Add(t_up);
+        Box t_down = gameObject.AddComponent<Box>();
+        boxNeighbours.Add(t_down);
+        Box t_left = gameObject.AddComponent<Box>();
+        boxNeighbours.Add(t_left);
+        Box t_right = gameObject.AddComponent<Box>();
+        boxNeighbours.Add(t_right);
+
+        t_up.m_posX = m_newOriginBox.m_up.x;
+        t_up.m_posZ = m_newOriginBox.m_up.y;
+
+        t_down.m_posX = m_newOriginBox.m_down.x;
+        t_down.m_posZ = m_newOriginBox.m_down.y;
+
+        t_left.m_posX = m_newOriginBox.m_left.x;
+        t_left.m_posZ = m_newOriginBox.m_left.y;
+
+        t_right.m_posX = m_newOriginBox.m_right.x;
+        t_right.m_posZ = m_newOriginBox.m_right.y;
+
+        float minorManh = 9999;
+        Box currentPath = new Box(); //gameObject.AddComponent<Box>();
+
+        for (int i = 0; i < boxNeighbours.Count; ++i)
+        {
+            float t_manh = ManhattanDist(boxNeighbours[i]);
+            if (minorManh > t_manh)
+            {
+                minorManh = t_manh;
+                currentPath = boxNeighbours[i];
+            }
+        }
+        m_newOriginBox = currentPath;
+
+        m_newOriginBox.m_pathMember = true;
+        boxNeighbours.Clear();
+
+        for (int i = 0; i < m_gridBoxInstances.Count; ++i)
+        {
+            //if (m_gridBoxInstances[i].m_posX == currentPath.m_posX && m_gridBoxInstances[i].m_posZ == currentPath.m_posZ)
+            if (m_gridBoxInstances[i].m_pathMember)
+            {
+                m_gridBoxInstances[i].GetComponent<MeshRenderer>().material = m_routeMat;
+            }
+        }
+     */
+    void FindPath()
+    {
+        List<Vector2> boxNeighbours = new List<Vector2>();
+        Vector2 t_up = new Vector2(0, 0);
+        Vector2 t_down = new Vector2(0, 0);
+        Vector2 t_left = new Vector2(0, 0);
+        Vector2 t_right = new Vector2(0, 0);
+
+        t_up.y = m_newOriginBox.m_posZ;
+        t_up.x = m_newOriginBox.m_posX + 1f;
+
+        t_down.y = m_newOriginBox.m_posZ;
+        t_down.x = m_newOriginBox.m_posX - 1f;
+
+        t_left.y = m_newOriginBox.m_posZ - 1f;
+        t_left.x = m_newOriginBox.m_posX;
+
+        t_right.y = m_newOriginBox.m_posZ + 1f;
+        t_right.x = m_newOriginBox.m_posX;
+
+        boxNeighbours.Add(t_up);
+        boxNeighbours.Add(t_down);
+        boxNeighbours.Add(t_left);
+        boxNeighbours.Add(t_right);
+        float minorManh = 9999;
+        Vector2 currentPath = new Vector2(0,0); //gameObject.AddComponent<Box>();
+
+        for (int i = 0; i < boxNeighbours.Count; ++i)
+        {
+            float t_manh = ManhattanDistVector(boxNeighbours[i]);
+            if (minorManh > t_manh)
+            {
+                minorManh = t_manh;
+                currentPath.x = boxNeighbours[i].x;
+                currentPath.y = boxNeighbours[i].y;
+            }
+        }
+        m_newOriginBox.m_posX = currentPath.x;
+        m_newOriginBox.m_posZ = currentPath.y;
+
+        m_newOriginBox.m_pathMember = true;
+        boxNeighbours.Clear();
+
+        for (int i = 0; i < m_gridBoxInstances.Count; ++i)
+        {
+            //if (m_gridBoxInstances[i].m_posX == currentPath.m_posX && m_gridBoxInstances[i].m_posZ == currentPath.m_posZ)
+            if (m_gridBoxInstances[i].m_posX == m_newOriginBox.m_posX && m_gridBoxInstances[i].m_posZ == m_newOriginBox.m_posZ)
+            {
+                Box temp = m_gridBoxInstances[i];
+                Vector3 vect = new Vector3(temp.transform.position.x, temp.transform.position.y, temp.transform.position.z);
+                temp.transform.position += vect;
+                m_gridBoxInstances[i] = temp;
+                GameObject gridMesh = Instantiate(m_gridBox, new Vector3(temp.m_posX, temp.transform.position.y+0.5f, temp.m_posZ), Quaternion.identity);
+                gridMesh.GetComponent<MeshRenderer>().material = m_routeMat;
+            }
+        }
+        //m_originBox = m_newOriginBox;
     }
 
     void InstantiateGrid( )
     {
+        // searches random points for origin and objective
+        m_objectiveX = Random.Range(0, m_X);
+        m_objectiveZ = Random.Range(0, m_Z);
+
+        m_originX = Random.Range(0, m_X);
+        m_originZ = Random.Range(0, m_Z);
+
+        while (m_originX == m_objectiveX)
+        {
+            m_originX = Random.Range(0, m_X);
+        }
+        while (m_originZ == m_objectiveZ)
+        {
+            m_originZ = Random.Range(0, m_Z);
+        }
+
+        // instantiates all the boxes of the grid
         int counter = 0;
-        for (int i = 0; i < m_Y; ++i)
+        for (int i = 0; i < m_Z; ++i)
         {
             int xCounter = 0;
             for (int j = 0; j < m_X; ++j)
@@ -50,26 +190,85 @@ public class grid : MonoBehaviour
 
                     GameObject gridMesh = Instantiate(m_gridBox, new Vector3(transform.position.x + xCounter, transform.position.y, transform.position.z + counter), Quaternion.identity);
                     gridMesh.GetComponent<MeshRenderer>().material = m_objectiveMat;
-                    m_gridBoxInstances.Add(gridMesh);
+                    gridMesh.GetComponent<Box>().m_posX = j;
+                    gridMesh.GetComponent<Box>().m_posZ = i;
+                    Box originBox = gameObject.AddComponent<Box>();
+                    originBox.m_posX = j;
+                    originBox.m_posZ = i;
+                    //m_originBox = originBox;
+                    m_gridBoxInstances.Add(originBox);
                 }
                 else if (m_originX == j && m_originZ == i)
                 {
                     GameObject gridMesh = Instantiate(m_gridBox, new Vector3(transform.position.x + xCounter, transform.position.y, transform.position.z + counter), Quaternion.identity);
                     gridMesh.GetComponent<MeshRenderer>().material = m_originMat;
-                    m_gridBoxInstances.Add(gridMesh);
+                    gridMesh.GetComponent<Box>().m_posX = j;
+                    gridMesh.GetComponent<Box>().m_posZ = i;
+                    Box originBox = gameObject.AddComponent<Box>();
+                    originBox.m_posX = j;
+                    originBox.m_posZ = i;
+                    m_originBox = originBox;
+                    m_gridBoxInstances.Add(originBox);
                 }
                 else
                 {
                     GameObject gridMesh = Instantiate(m_gridBox, new Vector3(transform.position.x + xCounter, transform.position.y, transform.position.z + counter), Quaternion.identity);
                     gridMesh.GetComponent<MeshRenderer>().material = m_neutralMat;
-                    m_gridBoxInstances.Add(gridMesh);
+                    gridMesh.GetComponent<Box>().m_posX = j;
+                    gridMesh.GetComponent<Box>().m_posZ = i;
+                    Box originBox = gameObject.AddComponent<Box>();
+                    originBox.m_posX = j;
+                    originBox.m_posZ = i;
+                    //m_originBox = originBox;
+                    m_gridBoxInstances.Add(originBox);
                 }
                 xCounter += 1;
             }
             counter += 1;
         }
+
+        // manhattan distance
+        ManhattanDist(m_originBox);
+
+        m_originSolid.x = m_originX;
+        m_originSolid.y = m_originZ;
+
+        // path searching
+        SearchPath();
     }
 
+
+    float ManhattanDist(Box point)
+    {
+        return (Mathf.Abs(point.m_posX - m_objectiveX) + Mathf.Abs(point.m_posZ - m_objectiveZ)) ;
+    }
+
+    float ManhattanDistVector(Vector2 point)
+    {
+        return (Mathf.Abs(point.x - m_objectiveX) + Mathf.Abs(point.y - m_objectiveZ)) ;
+    }
+
+    //path searching
+    void SearchPath()
+    {
+        m_h = Mathf.Abs(m_objectiveX - m_originX) + Mathf.Abs(m_objectiveZ - m_originZ);
+        //IEnumerable<GameObject> m_possibleBoxes;
+        {
+            foreach (Box box in m_gridBoxInstances)
+            {
+                if (   box.GetComponent<Box>().m_posX == m_originX -1 
+                    || box.GetComponent<Box>().m_posX == m_originX + 1
+                    || box.GetComponent<Box>().m_posZ == m_originZ + 1
+                    || box.GetComponent<Box>().m_posZ == m_originZ - 1)
+                {
+                    //yield return box;
+                }
+            }
+            //yield break;
+        }
+        m_f = m_g + m_h;
+
+    }
     void Search()
     {
         int searchX = 0;
